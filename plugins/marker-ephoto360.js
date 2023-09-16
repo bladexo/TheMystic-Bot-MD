2fb90e81b41b511413738d81dc86766bbafc8a0e
blade_xo <89949214+bladexo@users.noreply.github.com>
2023-09-16 01:17:38 +0100

    Update marker-ephoto360.js

--- plugins/marker-ephoto360.js
+++ plugins/marker-ephoto360.js
@@ -14,13 +14,13 @@ const handler = async (m, {conn, args, command, usedPrefix}) => {
   if (command == 'logochristmas') {
     try {
       await conn.reply(m.chat, '*[❗] 𝙴𝙻𝙰𝙱𝙾𝚁𝙰𝙽𝙳𝙾 𝚂𝚄 𝙳𝙸𝚂𝙴𝙽̃𝙾, 𝙴𝚂𝙿𝙴𝚁𝙴 𝚄𝙽 𝙼𝙾𝙼𝙴𝙽𝚃𝙾...*', m);
-      const res2 = await new Maker().Ephoto360('https://en.ephoto360.com/christmas-effect-by-name-376.html', [response[0]]);
+      const res2 = await new Maker().Ephoto360('https://en.ephoto360.com/create-the-titanium-text-effect-to-introduce-iphone-15-812.html', [response[0]]);
       await conn.sendFile(m.chat, res2.imageUrl, 'error.jpg', null, m);
     } catch {
       await conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*', m);
     }
   }
-if (command == 'logoephoto360') {
+/* if (command == 'logoephoto360') {
 if (!response[0].includes('ephoto360')) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙰 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 "𝙴𝙿𝙷𝙾𝚃𝙾𝟹𝟼0", 𝙼𝙰𝚂 𝙴𝙻 𝚃𝙴𝚇𝚃𝙾 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙰𝙶𝚁𝙴𝙶𝙰𝚁*
 
 *—◉ 𝙴𝙹𝙴𝙼𝙿𝙻𝙾𝚂:*
